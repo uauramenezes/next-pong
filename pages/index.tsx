c788a2086e4bb5985377ede61e2964a6ea0775c4
uauramenezes <uaurasilva@hotmail.com>
2021-08-01 10:40:29 -0300

    Add canvas element

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,18 @@
-import Head from 'next/head'
+import Head from 'next/head';
+import { useEffect } from 'react';
+import Pong from '../scripts/Pong';
 
-export default function Pong() {
+export default function Home() {
+  useEffect(() => {
+    Pong()
+  }, [])
   return (
-    <main className='main'>
+    <main id='main'>
       <Head>
         <title>Pong</title>
         <meta name="description" content="A simple Pong game built with Next.js" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <canvas id="canvas" width='700' height='400'></canvas>
     </main>
   )
